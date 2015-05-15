47f4b12282de0c46c3cd93580ef1d2c9dc71cbf8
TanabeMai <at11d.tm919@gmail.com>
2015-05-15 14:25:47 +0900

    インポート編集

--- gameProgram/import.cpp
+++ gameProgram/import.cpp
@@ -20,20 +20,10 @@ const char* TEX_PATH[] =
 	"./data/TEXTURE/fade.jpg",
 };
 
-// Xファイル
-const char* X_PATH[] =
-{
-	NULL,
-};
-
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // 静的変数
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 LPDIRECT3DTEXTURE9	CImport::m_tex[TEX_MAX];
-XFILE				CImport::m_xFile[X_MAX];
-
-D3DXVECTOR3 CImport::m_VtxMin[];
-D3DXVECTOR3 CImport::m_VtxMax[];
 
 //=============================================================================
 // コンストラクタ
@@ -44,19 +34,6 @@ CImport::CImport(void)
 	for(int cnt = 0; cnt < TEX_MAX; ++cnt)
 	{
 		m_tex[cnt] = nullptr;
-	}
-
-	// Xファイル
-	for(int cnt = 0; cnt < TEX_MAX; ++cnt)
-	{
-		m_xFile[cnt] = XFILE();
-	}
-
-	// サイズ
-	for(int cnt = 0; cnt < TEX_MAX; ++cnt)
-	{
-		m_VtxMin[cnt] = D3DXVECTOR3(0.0f,0.0f,0.0f);
-		m_VtxMax[cnt] = D3DXVECTOR3(0.0f,0.0f,0.0f);
 	}
 }
 
@@ -83,48 +60,6 @@ HRESULT CImport::Init(LPDIRECT3DDEVICE9 device)
 		D3DXCreateTextureFromFile(device, TEX_PATH[cnt], &m_tex[cnt]);
 	}
 
-	//----------------------------
-	// Xファイル
-	//----------------------------
-	for(int cnt = 1; cnt < X_MAX; ++cnt)
-	{
-		// モデル読み込み
-		if(FAILED(D3DXLoadMeshFromX(X_PATH[cnt],
-									D3DXMESH_SYSTEMMEM,
-									device,
-									NULL,
-									&m_xFile[cnt].buffMat,
-									NULL,
-									&m_xFile[cnt].numMat,
-									&m_xFile[cnt].mesh)))
-		{
-			continue;
-		}
-
-		// マテリアルの設定
-		m_xFile[cnt].mat = (D3DXMATERIAL *)m_xFile[cnt].buffMat->GetBufferPointer();	// マテリアルの先頭アドレス取得
-		m_xFile[cnt].tex = new LPDIRECT3DTEXTURE9[m_xFile[cnt].numMat];
-
-		for(unsigned int cntMat = 0; cntMat < m_xFile[cnt].numMat; cntMat++)
-		{
-			//テクスチャをロード
-			m_xFile[cnt].tex[cntMat] = NULL;
-
-			if(m_xFile[cnt].mat[cntMat].pTextureFilename != NULL)
-			{
-				HRESULT hr = D3DXCreateTextureFromFile(device, m_xFile[cnt].mat[cntMat].pTextureFilename, &m_xFile[cnt].tex[cntMat]);
-				if(FAILED(hr))
-				{
-					m_xFile[cnt].tex[cntMat] = NULL;
-				}
-			}
-		}
-
-		// サイズの取得
-		m_VtxMin[cnt] = GetVtxMin(cnt);
-		m_VtxMax[cnt] = GetVtxMax(cnt);
-	}
-
 	return S_OK;
 }
 
@@ -134,21 +69,6 @@ HRESULT CImport::Init(LPDIRECT3DDEVICE9 device)
 void CImport::Uninit(void)
 {
 	//----------------------------
-	// Xファイル
-	//----------------------------
-	for(int cnt = 1; cnt < X_MAX; cnt++)
-	{
-		// テクスチャポインタ解放
-		SAFE_DELETE(m_xFile[cnt].tex);
-
-		// メッシュ情報の開放
-		SAFE_RELEASE(m_xFile[cnt].mesh);
-
-		// マテリアル情報の開放
-		SAFE_RELEASE(m_xFile[cnt].buffMat);
-	}
-
-	//----------------------------
 	// テクスチャ
 	//----------------------------
 	for(int cnt = 1; cnt < TEX_MAX; cnt++)
@@ -156,223 +76,4 @@ void CImport::Uninit(void)
 		// テクスチャの開放
 		SAFE_RELEASE(m_tex[cnt]);
 	}
-}
-
-
-//=============================================================================
-// 頂点座標を取得(最小値)
-// 引数:無
-// 戻値:無
-//=============================================================================
-D3DXVECTOR3 CImport::GetVtxMin(int index)
-{
-	D3DXVECTOR3 pos[2];
-	BYTE *pVertexBuffer;								// 頂点バッファ
-	WORD *pIndexBuffer;									// インデックスバッファ
-	D3DXVECTOR3 Vertex;									// 頂点
-	DWORD NumFaces = m_xFile[index].mesh->GetNumFaces();		// ポリゴン数の取得
-	DWORD NumVertex = m_xFile[index].mesh->GetNumVertices();	// 頂点数の取得
-	DWORD FVF = m_xFile[index].mesh->GetFVF();					// 頂点フォーマットを取得
-	DWORD VertexSize = D3DXGetFVFVertexSize(FVF);		// 頂点サイズの取得
-	
-	// ロック（頂点バッファ）
-	// ロック（インデックスバッファ）
-	m_xFile[index].mesh->LockVertexBuffer(D3DLOCK_READONLY,(LPVOID*)&pVertexBuffer);
-	m_xFile[index].mesh->LockIndexBuffer(D3DLOCK_READONLY, (LPVOID*)&pIndexBuffer);
-	
-	// ポリゴン数だけループ
-	DWORD VerNum[3];				// 頂点番号(インデックス)
-	D3DXVECTOR3 VerPos[3];				// 頂点座標
-	D3DXVECTOR3 VerPos2[3];			// 頂点座標
-	D3DXVECTOR3 Ver[3];				// 頂点座標
-
-	// 頂点番号取得(何番目の頂点化)
-	VerNum[0] = pIndexBuffer[0];	// 頂点1
-	VerNum[1] = pIndexBuffer[1];	// 頂点2
-	VerNum[2] = pIndexBuffer[2];	// 頂点3
-	
-	// 頂点座標取得
-	VerPos[0] = VerPos2[0] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[0]*VertexSize);
-	VerPos[1] = VerPos2[1] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[1]*VertexSize);
-	VerPos[2] = VerPos2[2] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[2]*VertexSize);
-
-	Ver[0] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[0]*VertexSize);
-	Ver[1] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[1]*VertexSize);
-	Ver[2] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[2]*VertexSize);
-
-	for( unsigned int i = 0; i < NumFaces; i++ )
-	{
-		// 頂点番号取得(何番目の頂点化)
-		VerNum[0] = pIndexBuffer[i*3 + 0];	// 頂点1
-		VerNum[1] = pIndexBuffer[i*3 + 1];	// 頂点2
-		VerNum[2] = pIndexBuffer[i*3 + 2];	// 頂点3
-	
-		// 頂点座標取得
-		Ver[0] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[0]*VertexSize);
-		Ver[1] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[1]*VertexSize);
-		Ver[2] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[2]*VertexSize);
-		
-		if( VerPos[0].x > Ver[0].x )
-		{
-			VerPos[0].x = Ver[0].x;
-		}
-
-		if( VerPos[1].x > Ver[1].x )
-		{
-			VerPos[1].x = Ver[1].x;
-		}
-
-		if( VerPos[2].x > Ver[2].x )
-		{
-			VerPos[2].x = Ver[2].x;
-		}
-
-		if( VerPos[0].y > Ver[0].y )
-		{
-			VerPos[0].y = Ver[0].y;
-		}
-
-		if( VerPos[1].y > Ver[1].y )
-		{
-			VerPos[1].y = Ver[1].y;
-		}
-
-		if( VerPos[2].y > Ver[2].y )
-		{
-			VerPos[2].y = Ver[2].y;
-		}
-
-		if( VerPos[0].z > Ver[0].z )
-		{
-			VerPos[0].z = Ver[0].z;
-		}
-
-		if( VerPos[1].z > Ver[1].z )
-		{
-			VerPos[1].z = Ver[1].z;
-		}
-
-		if( VerPos[2].z > Ver[2].z )
-		{
-			VerPos[2].z = Ver[2].z;
-		}
-	}
-
-	pos[0] = VerPos[0];
-
-	// アンロック（頂点バッファ）
-	// アンロック（インデックスバッファ）
-	m_xFile[index].mesh->UnlockVertexBuffer();
-	m_xFile[index].mesh->UnlockIndexBuffer();
-
-	return pos[0];
-}
-
-//=============================================================================
-// 頂点座標を取得(最大値)
-// 引数:無
-// 戻値:無
-//=============================================================================
-D3DXVECTOR3 CImport::GetVtxMax(int index)
-{
-	D3DXVECTOR3 pos[2];
-	BYTE *pVertexBuffer;								// 頂点バッファ
-	WORD *pIndexBuffer;									// インデックスバッファ
-	D3DXVECTOR3 Vertex;									// 頂点
-	DWORD NumFaces = m_xFile[index].mesh->GetNumFaces();		// ポリゴン数の取得
-	DWORD NumVertex = m_xFile[index].mesh->GetNumVertices();	// 頂点数の取得
-	DWORD FVF = m_xFile[index].mesh->GetFVF();					// 頂点フォーマットを取得
-	DWORD VertexSize = D3DXGetFVFVertexSize(FVF);		// 頂点サイズの取得
-	
-	// ロック（頂点バッファ）
-	// ロック（インデックスバッファ）
-	m_xFile[index].mesh->LockVertexBuffer(D3DLOCK_READONLY,(LPVOID*)&pVertexBuffer);
-	m_xFile[index].mesh->LockIndexBuffer(D3DLOCK_READONLY, (LPVOID*)&pIndexBuffer);
-	
-	// ポリゴン数だけループ
-	DWORD VerNum[3];				// 頂点番号(インデックス)
-	D3DXVECTOR3 VerPos[3];				// 頂点座標
-	D3DXVECTOR3 VerPos2[3];			// 頂点座標
-	D3DXVECTOR3 Ver[3];				// 頂点座標
-
-	// 頂点番号取得(何番目の頂点化)
-	VerNum[0] = pIndexBuffer[0];	// 頂点1
-	VerNum[1] = pIndexBuffer[1];	// 頂点2
-	VerNum[2] = pIndexBuffer[2];	// 頂点3
-	
-	// 頂点座標取得
-	VerPos[0] = VerPos2[0] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[0]*VertexSize);
-	VerPos[1] = VerPos2[1] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[1]*VertexSize);
-	VerPos[2] = VerPos2[2] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[2]*VertexSize);
-
-	Ver[0] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[0]*VertexSize);
-	Ver[1] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[1]*VertexSize);
-	Ver[2] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[2]*VertexSize);
-
-	for( unsigned int i = 0; i < NumFaces; i++ )
-	{
-		// 頂点番号取得(何番目の頂点化)
-		VerNum[0] = pIndexBuffer[i*3 + 0];	// 頂点1
-		VerNum[1] = pIndexBuffer[i*3 + 1];	// 頂点2
-		VerNum[2] = pIndexBuffer[i*3 + 2];	// 頂点3
-	
-		// 頂点座標取得
-		Ver[0] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[0]*VertexSize);
-		Ver[1] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[1]*VertexSize);
-		Ver[2] = *(D3DXVECTOR3*)(pVertexBuffer + VerNum[2]*VertexSize);
-
-		if( VerPos2[0].x < Ver[0].x )
-		{
-			VerPos2[0].x = Ver[0].x;
-		}
-
-		if( VerPos2[1].x < Ver[1].x )
-		{
-			VerPos2[1].x = Ver[1].x;
-		}
-
-		if( VerPos2[2].x < Ver[2].x )
-		{
-			VerPos2[2].x = Ver[2].x;
-		}
-
-		if( VerPos2[0].y < Ver[0].y )
-		{
-			VerPos2[0].y = Ver[0].y;
-		}
-
-		if( VerPos2[1].y < Ver[1].y )
-		{
-			VerPos2[1].y = Ver[1].y;
-		}
-
-		if( VerPos2[2].y < Ver[2].y )
-		{
-			VerPos2[2].y = Ver[2].y;
-		}
-
-		if( VerPos2[0].z < Ver[0].z )
-		{
-			VerPos2[0].z = Ver[0].z;
-		}
-
-		if( VerPos2[1].z < Ver[1].z )
-		{
-			VerPos2[1].z = Ver[1].z;
-		}
-
-		if( VerPos2[2].z < Ver[2].z )
-		{
-			VerPos2[2].z = Ver[2].z;
-		}
-	}
-
-	pos[1] = VerPos2[0];
-
-	// アンロック（頂点バッファ）
-	// アンロック（インデックスバッファ）
-	m_xFile[index].mesh->UnlockVertexBuffer();
-	m_xFile[index].mesh->UnlockIndexBuffer();
-
-	return pos[1];
 }
--- gameProgram/import.h
+++ gameProgram/import.h
@@ -15,27 +15,6 @@
 #include "main.h"
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// 構造体定義
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-typedef struct XFILE
-{
-	XFILE()
-	{
-		buffMat	= nullptr;
-		numMat	= 0;
-		mesh	= nullptr;
-		mat		= nullptr;
-		tex		= nullptr;
-	}
-
-	LPD3DXBUFFER		buffMat;	// マテリアル情報へのポインタ
-	DWORD				numMat;		// マテリアル情報の数
-	LPD3DXMESH			mesh;		// メッシュ情報へのポインタ
-	D3DXMATERIAL*		mat;		// マテリアルデータ先頭ポインタ
-	LPDIRECT3DTEXTURE9*	tex;		// テクスチャデータ先頭ポインタ
-} XFILE;
-
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // クラス定義
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 class CImport
@@ -48,12 +27,6 @@ public:
 		TEX_MAX
 	} TEXTURES;
 
-	typedef enum
-	{
-		X_NONE = 0,
-		X_MAX
-	} XFILES;
-
 	CImport();
 	~CImport(){};
 
@@ -62,19 +35,9 @@ public:
 	void	Uninit(void);
 
 	static LPDIRECT3DTEXTURE9 GetTexture(TEXTURES tex){return m_tex[tex];}
-	static XFILE			  GetXFile(XFILES x){return m_xFile[x];}
-
-	static D3DXVECTOR3 GetVtxSizeMin(XFILES index){return m_VtxMin[index];}
-	static D3DXVECTOR3 GetVtxSizeMax(XFILES index){return m_VtxMax[index];}
-	static D3DXVECTOR3 GetVtxMin(int index);
-	static D3DXVECTOR3 GetVtxMax(int index);
 
 private:
-	static LPDIRECT3DTEXTURE9 m_tex[TEX_MAX];
-	static XFILE			  m_xFile[X_MAX];
-
-	static D3DXVECTOR3        m_VtxMin[X_MAX];
-	static D3DXVECTOR3        m_VtxMax[X_MAX];
+	static LPDIRECT3DTEXTURE9 m_tex[TEX_MAX];;
 };
 
 //=============================================================================
