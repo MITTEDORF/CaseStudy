2771e2a4beed9f0d9751fec6f04d662832814037
matsu <matsuuraken0226@gmail.com>
2015-06-02 12:25:44 +0900

    Merge branch 'master' of https://github.com/MITTEDORF/CaseStudy into dev/ths20735test
    Conflicts:
    	gameProgram/directX.suo

--- gameProgram/stumbler.h
+++ gameProgram/stumbler.h
@@ -31,9 +31,17 @@ public:
 	void	Update(void);
 	void	Draw(void);
 
+	// スクロール処理
 	void	Scroll(float scroll){m_pos.x -= scroll; CScene2D::SetVertexPolygon();}
+	// HPセット処理
+	void	SetHP(int life){m_life = life;}
+	// HPゲット処理
+	int		GetHP(void){return m_life;}
+	// 攻撃を受けた場合のHP減算処理
 	void	Attack(int damage){m_life -= damage;}
+	// 生存チェック
 	bool	LivingCheck(void);
+	// 衝突判定
 	bool	CheckCollisionAABB(D3DXVECTOR2 pos, D3DXVECTOR2 size, POINT_TYPE pType);
 
 protected:
