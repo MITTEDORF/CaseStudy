64587b75fd0f4c78deee54e8308f9fc6aefead69
matsu <matsuuraken0226@gmail.com>
2015-06-30 10:15:04 +0900

    とり

--- gameProgram/stumbler.cpp
+++ gameProgram/stumbler.cpp
@@ -18,6 +18,9 @@ CStumbler::CStumbler(int priority, OBJTYPE objType) : CScene2D(priority, objType
 	m_life = 1;
 	m_next = NULL;
 	m_prev = NULL;
+	m_move = -1.0f;
+	m_defpos = D3DXVECTOR2(0.0f, 0.0f);
+	m_texAnim = 0;
 }
 
 //=============================================================================
@@ -34,7 +37,7 @@ CStumbler* CStumbler::Create(LPDIRECT3DDEVICE9 device, STUM_DATA data, POINT_TYP
 		D3DXVECTOR2(128, 128),		// TYPE_LOG_LEFT
 		D3DXVECTOR2(128, 128),		// TYPE_LOG_CENTER
 		D3DXVECTOR2(128, 128),		// TYPE_LOG_RIGHT
-		D3DXVECTOR2(128, 128),		// TYPE_BIRD
+		D3DXVECTOR2(96, 96),		// TYPE_BIRD
 		D3DXVECTOR2(80, 96),		// TYPE_DUSTBOX
 		D3DXVECTOR2(128, 128),		// TYPE_BARRICADE
 		D3DXVECTOR2(128, 128)		// TYPE_MAX
@@ -59,9 +62,20 @@ CStumbler* CStumbler::Create(LPDIRECT3DDEVICE9 device, STUM_DATA data, POINT_TYP
 	pointer->Init(device, (CImport::TEXTURES)(CImport::SIGNBOARD + data.type), pointType);
 	// データを元に座標の変更
 	pointer->SetPos(data.Index.x * 64, SCREEN_HEIGHT - ((data.Index.y * 64) + 128));
+	// デフォルト位置セット処理
+	pointer->SetDefPos(pointer->GetPos());
+	// カラスの時にテクスチャUVの変更
+	if(data.type == TYPE_BIRD)
+	{
+		pointer->SetCord(0, D3DXVECTOR2(0, 0));
+		pointer->SetCord(1, D3DXVECTOR2(0.5, 0));
+		pointer->SetCord(2, D3DXVECTOR2(0, 1));
+		pointer->SetCord(3, D3DXVECTOR2(0.5, 1));
+	}
 	// 障害物タイプによる当たり判定の変更
 	pointer->SetHitSize(Size_List[data.type]);
 	pointer->SetHitOffset(Offset_List[data.type]);
+	pointer->SetStumType(data.type);
 	return pointer;
 }
 
@@ -108,6 +122,42 @@ void CStumbler::Update(void)
 		// 削除フラグ立てる
 		CScene::Delete();
 	}
+
+	// タイプによって色々処理
+	switch(m_type)
+	{
+	case TYPE_BIRD:			// カラスの時
+		// 上下移動
+		CScene2D::SetPosY(m_pos.y + m_move);
+		// デフォルト位置との開きが規定値を超えたら反転
+		if(abs(m_defpos.y - m_pos.y) > 45.0f)
+			m_move *= -1;
+		// 12fごとにテクスチャアニメーション
+		if(m_texAnim % 12 == 0)
+		{
+			if(m_coord[0].x != 0.5f)
+			{
+				CScene2D::SetCord(0, D3DXVECTOR2(0.5, 0));
+				CScene2D::SetCord(1, D3DXVECTOR2(1, 0));
+				CScene2D::SetCord(2, D3DXVECTOR2(0.5, 1));
+				CScene2D::SetCord(3, D3DXVECTOR2(1, 1));
+			}
+			else
+			{
+				CScene2D::SetCord(0, D3DXVECTOR2(0, 0));
+				CScene2D::SetCord(1, D3DXVECTOR2(0.5, 0));
+				CScene2D::SetCord(2, D3DXVECTOR2(0, 1));
+				CScene2D::SetCord(3, D3DXVECTOR2(0.5, 1));
+			}
+		}
+		break;
+
+	default:
+		break;
+	}
+
+	m_texAnim++;
+
 	// 継承元の更新処理呼び出し
 	CScene2D::Update();
 }
