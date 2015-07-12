31e57f48d9ff1214fb73ffd2b066745f13b2f382
TanabeMai <at11d.tm919@gmail.com>
2015-07-12 18:40:50 +0900

    一時コミット

--- gameProgram/inputPadX.cpp
+++ gameProgram/inputPadX.cpp
@@ -16,6 +16,7 @@
 #define REPEAT_MIN		(30)	// リピート開始カウント数
 #define REPEAT_BETWEEN	(3)		// リピート間隔カウント数
 #define BUTTON_NUM		(14)	// ボタンの数
+
 const WORD PAD_BUTTON[BUTTON_NUM] =
 {
 	XINPUT_GAMEPAD_DPAD_UP,
@@ -46,13 +47,37 @@ CInputPadX::CInputPadX(void)
 	m_connected = false;
 
 	// 入力情報
-	m_pad.Gamepad.wButtons		= 0;
-	m_pad.Gamepad.bLeftTrigger	= 0;
-	m_pad.Gamepad.bRightTrigger	= 0;
-	m_pad.Gamepad.sThumbLX		= 0;
-	m_pad.Gamepad.sThumbLY		= 0;
-	m_pad.Gamepad.sThumbRX		= 0;
-	m_pad.Gamepad.sThumbRY		= 0;
+	m_state.Gamepad.wButtons		= 0;
+	m_state.Gamepad.bLeftTrigger	= 0;
+	m_state.Gamepad.bRightTrigger	= 0;
+	m_state.Gamepad.sThumbLX		= 0;
+	m_state.Gamepad.sThumbLY		= 0;
+	m_state.Gamepad.sThumbRX		= 0;
+	m_state.Gamepad.sThumbRY		= 0;
+
+	m_trigger.Gamepad.wButtons		= 0;
+	m_trigger.Gamepad.bLeftTrigger	= 0;
+	m_trigger.Gamepad.bRightTrigger	= 0;
+	m_trigger.Gamepad.sThumbLX		= 0;
+	m_trigger.Gamepad.sThumbLY		= 0;
+	m_trigger.Gamepad.sThumbRX		= 0;
+	m_trigger.Gamepad.sThumbRY		= 0;
+
+	m_release.Gamepad.wButtons		= 0;
+	m_release.Gamepad.bLeftTrigger	= 0;
+	m_release.Gamepad.bRightTrigger	= 0;
+	m_release.Gamepad.sThumbLX		= 0;
+	m_release.Gamepad.sThumbLY		= 0;
+	m_release.Gamepad.sThumbRX		= 0;
+	m_release.Gamepad.sThumbRY		= 0;
+
+	m_repeat.Gamepad.wButtons		= 0;
+	m_repeat.Gamepad.bLeftTrigger	= 0;
+	m_repeat.Gamepad.bRightTrigger	= 0;
+	m_repeat.Gamepad.sThumbLX		= 0;
+	m_repeat.Gamepad.sThumbLY		= 0;
+	m_repeat.Gamepad.sThumbRX		= 0;
+	m_repeat.Gamepad.sThumbRY		= 0;
 }
 
 //=============================================================================
@@ -71,17 +96,20 @@ CInputPadX* CInputPadX::Create(void)
 HRESULT CInputPadX::Init(void)
 {
 	//----------------------------
-	// パッド情報取得
+	// パッド接続情報取得
 	//----------------------------
+	XINPUT_STATE pad;
+
 	for(DWORD cnt = 0; cnt < PAD_NUM; ++cnt)
 	{
-		if(XInputGetState(cnt, &m_pad) != ERROR_SUCCESS)
+		if(XInputGetState(cnt, &pad) != ERROR_SUCCESS)
 		{
+			// 繋がっていない
 			m_connected = false;
 		}
 		else
 		{
-			// 接続情報
+			// 繋がっている
 			m_no = cnt;
 			m_padFlg[m_no] = true;
 			m_connected	  = true;
@@ -108,14 +136,28 @@ void CInputPadX::Uninit(void)
 //=============================================================================
 void CInputPadX::Update(void)
 {
+	//----------------------------
 	// パッド情報更新
-	if(XInputGetState(m_no, &m_pad) != ERROR_SUCCESS)
+	//----------------------------
+	XINPUT_STATE pad;
+
+	if(XInputGetState(m_no, &m_state) != ERROR_SUCCESS)
 	{
+		// 繋がっていない
 		m_connected = false;
 	}
 	else
 	{
-		m_connected	  = true;
-	}
+		// 繋がっている
+		m_connected = true;
 
+//		//----------------------------
+//		// 入力情報更新
+//		//----------------------------
+//		// ボタン情報
+//		for(int cnt = 0; cnt < BUTTON_NUM; ++cnt)
+//		{
+//			// トリガー
+//		}
+	}
 }
--- gameProgram/inputPadX.h
+++ gameProgram/inputPadX.h
@@ -31,19 +31,23 @@ public:
 
 	bool GetPadFlg(void){return (m_no < 0) ? false : true;}
 
-	bool GetButton(WORD wButton){return (m_pad.Gamepad.wButtons & wButton) ? true : false;}
-	bool GetLeftTrigger(void){return (m_pad.Gamepad.bLeftTrigger > XINPUT_GAMEPAD_TRIGGER_THRESHOLD) ? true : false;}
-	bool GetRightrigger(void){return (m_pad.Gamepad.bRightTrigger > XINPUT_GAMEPAD_TRIGGER_THRESHOLD) ? true : false;}
+	bool GetButton(WORD wButton){return (m_state.Gamepad.wButtons & wButton) ? true : false;}
+	bool GetLeftTrigger(void){return (m_state.Gamepad.bLeftTrigger > XINPUT_GAMEPAD_TRIGGER_THRESHOLD) ? true : false;}
+	bool GetRightrigger(void){return (m_state.Gamepad.bRightTrigger > XINPUT_GAMEPAD_TRIGGER_THRESHOLD) ? true : false;}
 
-	SHORT GetThumbLX(void){return (abs(m_pad.Gamepad.sThumbLX) > XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) ? m_pad.Gamepad.sThumbLX : 0;}
-	SHORT GetThumbLY(void){return (abs(m_pad.Gamepad.sThumbLY) > XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) ? m_pad.Gamepad.sThumbLY : 0;}
-	SHORT GetThumbRX(void){return (abs(m_pad.Gamepad.sThumbRX) > XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE) ? m_pad.Gamepad.sThumbRX : 0;}
-	SHORT GetThumbRY(void){return (abs(m_pad.Gamepad.sThumbRY) > XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE) ? m_pad.Gamepad.sThumbRY : 0;}
+	SHORT GetThumbLX(void){return (abs(m_state.Gamepad.sThumbLX) > XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) ?  m_state.Gamepad.sThumbLX : 0;}
+	SHORT GetThumbLY(void){return (abs(m_state.Gamepad.sThumbLY) > XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE) ?  m_state.Gamepad.sThumbLY : 0;}
+	SHORT GetThumbRX(void){return (abs(m_state.Gamepad.sThumbRX) > XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE) ? m_state.Gamepad.sThumbRX : 0;}
+	SHORT GetThumbRY(void){return (abs(m_state.Gamepad.sThumbRY) > XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE) ? m_state.Gamepad.sThumbRY : 0;}
 
 private:
 	bool		 m_connected;
 	int			 m_no;
-	XINPUT_STATE m_pad;
+
+	XINPUT_STATE m_state;
+	XINPUT_STATE m_trigger;
+	XINPUT_STATE m_release;
+	XINPUT_STATE m_repeat;
 };
 
 //=============================================================================
